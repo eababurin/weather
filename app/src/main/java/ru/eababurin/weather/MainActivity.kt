29c5176b5b0091fd434693ca948e3caee8666d6f
Evgenii Baburin <r3drum@yandex.ru>
2022-06-24 07:33:13 +0300

    Вывел значения из разных циклов в консоль

--- app/src/main/java/ru/eababurin/weather/MainActivity.kt
+++ app/src/main/java/ru/eababurin/weather/MainActivity.kt
@@ -2,20 +2,25 @@ package ru.eababurin.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.constraintlayout.widget.Group
 import androidx.core.view.isVisible
 
+const val TAG = "TAG"
+
 data class User(val firstName: String, val lastName: String, val age: Int)
 
 class MainActivity : AppCompatActivity() {
 
-    val data = User("Евгений", "Бабурин", 29)
+    private val data = User("Евгений", "Бабурин", 29)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        start()
 
         fillFields(data)
 
@@ -40,5 +45,24 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.first_name).text = copyUser.firstName
         findViewById<TextView>(R.id.last_name).text = copyUser.lastName
         findViewById<TextView>(R.id.age).text = copyUser.age.toString()
+    }
+
+    private fun start() {
+
+        for (i in 1..10) {
+
+            when (i) {
+                in 4..7 -> Log.d(TAG, "В середине цикла; внутри when, i = $i")
+            }
+
+            while (i <= 3) {
+                Log.d(TAG, "В начале цикла; внутри while, i = $i"); break
+            }
+
+            do {
+                if (i <= 7) break
+                Log.d(TAG, "В конце цикла; внутри do-while, i = $i"); break
+            } while (i >= 7)
+        }
     }
 }
