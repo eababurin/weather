07a9e6c2b868b75c54dbeba5d6a21cf441dad3d5
Evgenii Baburin <r3drum@yandex.ru>
2022-07-09 05:01:56 +0300

    Добавил try/catch к загрузке и закрытие соединения

--- app/src/main/java/ru/eababurin/weather/repositories/RepositoryRemoteImpl.kt
+++ app/src/main/java/ru/eababurin/weather/repositories/RepositoryRemoteImpl.kt
@@ -1,27 +1,38 @@
 package ru.eababurin.weather.repositories
 
+import android.util.Log
 import com.google.gson.Gson
 import ru.eababurin.weather.BuildConfig
 import ru.eababurin.weather.model.dto.WeatherDTO
 import ru.eababurin.weather.utils.getLines
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
 
 class RepositoryRemoteImpl : OneResultWeatherGetable {
+    private val TAG = "TAG"
+
     override fun getWeather(lat: Double, lon: Double, block: (weather: WeatherDTO) -> Unit) {
         val url = URL("https://api.weather.yandex.ru/v2/informers?lat=$lat&lon=$lon")
-
         val connection = url.openConnection() as HttpURLConnection
-        connection.readTimeout = 5000
-        connection.addRequestProperty("X-Yandex-API-Key", BuildConfig.WEATHER_API_KEY)
 
-        Thread {
-            val reader = BufferedReader(InputStreamReader(connection.inputStream))
-            val weatherDTO: WeatherDTO = Gson().fromJson(getLines(reader), WeatherDTO::class.java)
+        try {
+            connection.readTimeout = 5000
+            connection.addRequestProperty("X-Yandex-API-Key", BuildConfig.WEATHER_API_KEY)
 
-            block(weatherDTO)
-        }.start()
+            Thread {
+                val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                val weatherDTO: WeatherDTO =
+                    Gson().fromJson(getLines(reader), WeatherDTO::class.java)
+
+                block(weatherDTO)
+            }.start()
+        } catch (e: Exception) {
+            Log.d(TAG, e.toString())
+        } finally {
+            connection.disconnect()
+        }
     }
 }
